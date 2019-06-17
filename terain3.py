f28ed9357979fdbe1df45a40419c93c2d5c8de25
mannus <mannus.schomaker@gmail.com>
2019-06-17 18:26:18 +0200

    added 3 new terrain generators

---
+++ terain3.py
@@ -0,0 +1,23 @@
+import noise
+import numpy as np
+from scipy.misc import toimage
+
+shape = (300,300)
+scale = 100.0
+octaves = 6
+persistence = 0.5
+lacunarity = 2.0
+
+world = np.zeros(shape)
+for i in range(shape[0]):
+    for j in range(shape[1]):
+        world[i][j] = noise.pnoise2(i/scale, 
+                                    j/scale, 
+                                    octaves=octaves, 
+                                    persistence=persistence, 
+                                    lacunarity=lacunarity, 
+                                    repeatx=1024, 
+                                    repeaty=1024, 
+                                    base=0)
+        
+toimage(world).show()
