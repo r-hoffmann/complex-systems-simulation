8ba7c0d461cae29e49ae64ae912f4cdcbd742b30
Dirk Zomerdijk <dirkzomerdijk@gmail.com>
2019-06-25 17:24:14 +0200

    3D Visual

--- TerrainPlotter.py
+++ TerrainPlotter.py
@@ -195,10 +195,6 @@ class TerrainPlotter(object):
             for cell in row:
                 terrain_heights_line.append(cell['terrain'])
                 peat_heights_line.append(cell['peat'])
-                # if cell['water'] > 0:
-                #     water_heights_line.append(1)
-                # else:
-                #     water_heights_line.append(0)
                 water_heights_line.append(cell['water'])
 
             terrain_heights.append(terrain_heights_line)
@@ -209,29 +205,84 @@ class TerrainPlotter(object):
         peat_heights = np.array(peat_heights)
         water_heights = np.array(water_heights)
 
+        hard_terrain_heights = terrain_heights + peat_heights
+
+        all_terrain = np.zeros((100, 100))
+        print(all_terrain.shape)
+        for x, row in enumerate(water_heights):
+            for y, water in enumerate(row):
+                if water > 0:
+                    all_terrain[x][y] = water + hard_terrain_heights[x][y]
+                else:
+                    all_terrain[x][y] = hard_terrain_heights[x][y] - 0.5
+        
+        print(all_terrain.shape)
+
+                
+            
+
         # make a color map of fixed colors
         cmap_terrain = mpl.cm.autumn_r
         norm_terrain = mpl.colors.Normalize(vmin=terrain_heights.min(), vmax=terrain_heights.max())
         
-        cmap_peat = mpl.cm.Greens
-        norm_peat = mpl.colors.Normalize(vmin=peat_heights.min(), vmax=peat_heights.max())
+        # cmap_peat = mpl.cm.Greens
+        # norm_peat = mpl.colors.Normalize(vmin=peat_heights.min(), vmax=peat_heights.max())
 
-        cmap_water = mpl.cm.Blues
-        norm_water = mpl.colors.Normalize(vmin=0, vmax=.5)
-                                            
-        fig, (ax1, ax2, ax3) = plt.subplots(figsize=(12, 3), ncols=3)
+        # cmap_water = mpl.cm.Blues
+        # norm_water = mpl.colors.Normalize(vmin=0, vmax=.5)
 
-        ax1.set_title('Height of terrain')
-        pos1 = ax1.imshow(terrain_heights, interpolation='nearest', cmap=cmap_terrain, norm=norm_terrain)
-        fig.colorbar(pos1, ax=ax1)
+
+        # fig3D, (ax1, ax2, ax3) = plt.subplots(figsize=(12, 3), ncols=3)
+
+        cmap_terrain = mpl.cm.Greens_r
+        norm_terrain = mpl.colors.Normalize(vmin=hard_terrain_heights.min(), vmax=hard_terrain_heights.max())
         
-        ax2.set_title('Height of peat')
-        pos2 = ax2.imshow(peat_heights, interpolation='nearest', cmap=cmap_peat, norm=norm_peat)
-        fig.colorbar(pos2, ax=ax2)
+        cmap_water_all_terrain = mpl.cm.Blues
+        norm_water_all_terrain = mpl.colors.Normalize(vmin=0, vmax=5)
+
+        X = np.arange(0,100)
+        Y = np.arange(0,100)
+        Z = hard_terrain_heights
+        Z_water = all_terrain
+
+        fig3D = plt.figure()
+        ax1 = Axes3D(fig3D)
+        ax1.view_init(elev=10, azim=110)
+        ax1.contour3D(X, Y, Z, 1000, cmap=cmap_terrain)
+        ax1.contour3D(X, Y, Z_water, 1000, c=water_heights, cmap=cmap_water_all_terrain, norm=norm_water_all_terrain)
+
+
+
+        # print(X.shape, Y.shape, Z.shape)
+        # ax1.scatter(xs=X, ys=Y, zs=Z_water)
+
+        ax1.set_title('3D Terrain Visualization')
+        ax1.grid(False)
+        ax1.set_zlim3d(0, 50)
+
+        # ax2 = plt.axes(projection='3d')
+        # ax2.contour3D(np.arange(0,100), np.arange(0,100), peat_heights, 50, cmap=cmap_peat)
+        # ax2.set_title('peat')
+
+        # ax3 = plt.axes(projection='3d')
+        # ax3.contour3D(np.arange(0,100), np.arange(0,100), terrain_heights, 50, cmap=cmap_terrain)
+        # ax3.set_title('terrain')
+
+
+        # plt.show()
+        # fig, (ax1, ax2, ax3) = plt.subplots(figsize=(12, 3), ncols=3)
+
+        # ax1.set_title('Height of terrain')
+        # pos1 = ax1.imshow(terrain_heights, interpolation='nearest', cmap=cmap_terrain, norm=norm_terrain)
+        # fig.colorbar(pos1, ax=ax1)
         
-        ax3.set_title('Height of water')
-        pos3 = ax3.imshow(water_heights, interpolation='nearest', cmap=cmap_water, norm=norm_water)
-        fig.colorbar(pos3, ax=ax3)
+        # ax2.set_title('Height of peat')
+        # pos2 = ax2.imshow(peat_heights, interpolation='nearest', cmap=cmap_peat, norm=norm_peat)
+        # fig.colorbar(pos2, ax=ax2)
+        
+        # ax3.set_title('Height of water')
+        # pos3 = ax3.imshow(water_heights, interpolation='nearest', cmap=cmap_water, norm=norm_water)
+        # fig.colorbar(pos3, ax=ax3)
 
         if self.save_to_filesystem:
             plt.savefig('images/{:05}.png'.format(t))
