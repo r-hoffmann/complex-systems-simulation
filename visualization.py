5705b0d33359676db8a00a5ba5738245053f2db7
Dirk Zomerdijk <dirkzomerdijk@gmail.com>
2019-06-27 21:42:50 +0200

    Go plot some towns

--- visualization.py
+++ visualization.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if len(sys.argv)>1:
         plotter = TerrainPlotter(filename='output_{}.json'.format(sys.argv[1]), save_to_filesystem=True, show=False)
     else:
-        plotter = TerrainPlotter(filename='output_34a.json', save_to_filesystem=True, show=False)
+        plotter = TerrainPlotter(filename='output_6.json', save_to_filesystem=True, show=True)
 
-    plotter.plot_3d()
+    plotter.plot_all_heights_with_measures(last=True)
